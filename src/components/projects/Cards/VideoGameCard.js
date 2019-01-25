d0824b0fd4f79a37927b8cf6f258e9bbfa952679
mankaine <wkhaine97@gmail.com>
2019-01-24 22:15:44 -0800

    Includes link to download and play video game

--- src/components/projects/Cards/VideoGameCard.js
+++ src/components/projects/Cards/VideoGameCard.js
@@ -3,13 +3,23 @@ import Card from '../../common/FeaturedProjectCard'
 import VideoGameCardArt from './VideoGameCardArt'
 
 const VideoGameCard = props => {
+  const bulb = [
+    {
+      className: 'project-link',
+      url: 'https://drive.google.com/drive/u/1/folders/1C5sTDhG2i7QPKfJDvtlF7OW3pPgMqCXy',
+      src: './images/link-symbol.svg',
+      alt: 'link'
+    }
+  ]
   const art = < VideoGameCardArt />
+
   return(
     <Card
       title="Dude, Where's My Ship?"
       description='A mystery/RPG video game about an explorer nagivating  an island full of monsters, and mountains. Suddenly, his ship vanishes out of thin air! Story and features made by myself and four others.'
       techStack='C# - Unity'
       art={art}
+      bulbs={bulb}
     />
   )
 }
